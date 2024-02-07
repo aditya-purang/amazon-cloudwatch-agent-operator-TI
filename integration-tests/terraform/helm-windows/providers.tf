fcfe78a8e7b86977d6c90d273e15fadd86dd568a
Kulwant Singh <ikulwant@amazon.com>
2024-02-06 22:15:46 -0800

    added manifest as experimental in kubernetes provider

--- integration-tests/terraform/helm-windows/providers.tf
+++ integration-tests/terraform/helm-windows/providers.tf
@@ -12,6 +12,9 @@ provider "helm" {
 }
 
 provider "kubernetes" {
+  experiments {
+    manifest_resource = true
+  }
   host                   = aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_windows_cluster_ca.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
